1bbb6afb9f5f0db3a6694b1f0cc949f373f091b9
rodell <ryanodell2668@gmail.com>
2023-02-27 15:13:31 -0500

    Returning list of students into JSON format with REST API

--- src/main/java/com/ryanpodell/sbblogrestapi/controller/StudentController.java
+++ src/main/java/com/ryanpodell/sbblogrestapi/controller/StudentController.java
@@ -3,17 +3,30 @@ import com.ryanpodell.sbblogrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
     // http://localhost:8080/student
-    @GetMapping("/student")
+    @GetMapping("student")
     public Student getStudent(){
         Student student = new Student(
                 1,
                 "Ryan",
                 "Tompson"
         );
-        return student;
+        return student;  //returns this object as a JSON object
+    }
+
+    //Want to return list of students as REST API
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(2, "Ryan", "Tommy"));
+        students.add(new Student(2, "Dave", "Smith"));
+        students.add(new Student(2, "Adam", "Charles"));
+        return students;
     }
 }
