c4e5891021f487d2d67028dd25ab17a2946ae185
rodell <ryanodell2668@gmail.com>
2023-02-28 13:46:12 -0500

    Using ResponseEntity in the Rest API's

--- src/main/java/com/ryanpodell/sbblogrestapi/controller/StudentController.java
+++ src/main/java/com/ryanpodell/sbblogrestapi/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.ryanpodell.sbblogrestapi.controller;
 import com.ryanpodell.sbblogrestapi.bean.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,58 +11,66 @@ public class StudentController {
 
     // http://localhost:8080/student
     @GetMapping("student")
-    public Student getStudent(){
+    public ResponseEntity<Student> getStudent(){
         Student student = new Student(
                 1,
                 "Ryan",
                 "Tompson"
         );
-        return student;  //returns this object as a JSON object
+        //return new ResponseEntity<>(student, HttpStatus.OK);  //returns this object as a JSON object
+        return ResponseEntity.ok().header(
+                "custom-header",
+                "ryanHeaderValue")
+                .body(student); //same as line above
     }
 
     //Want to return list of students as REST API
     @GetMapping("students")
-    public List<Student> getStudents(){
+    public ResponseEntity<List<Student>> getStudents(){
         List<Student> students = new ArrayList<>();
         students.add(new Student(2, "Ryan", "Tommy"));
         students.add(new Student(2, "Dave", "Smith"));
         students.add(new Student(2, "Adam", "Charles"));
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     //Want to set up path variable with method
     //{id} - URI template variable
     //http://localhost:8080/students/1
     @GetMapping("students/{id}")
-    public Student studentPathVariable(@PathVariable("id") int StudentId){
-        return new Student(StudentId, "Ryan", "LastName");
+    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int StudentId){
+        Student student = new Student(StudentId, "Ryan", "LastName");
+        return ResponseEntity.ok(student);
     }
 
     //http://localhost:8080/students/1/ryan/odell
     @GetMapping("students/{id}/{first-name}/{last-name}")
-    public Student studentFullPathVariable(
+    public ResponseEntity<Student> studentFullPathVariable(
             @PathVariable("id") int StudentId,
             @PathVariable("first-name") String firstName,
             @PathVariable("last-name") String lastName){
-        return new Student(StudentId, firstName, lastName);
+        Student student = new Student(StudentId, firstName, lastName);
+        return ResponseEntity.ok(student);
     }
 
     //Spring boot REST API with Request Param
     //http://localhost:8080/students?id=1
     @GetMapping("students/query?id=1")
-    public Student studentRequestVariable(@RequestParam int id){
-        return new Student(id, "StudentFirstName", "StudentLastName");
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int id){
+        Student student = new Student(id, "StudentFirstName", "StudentLastName");
+        return ResponseEntity.ok(student);
     }
 
     //Spring boot REST API with multiple Request Param
     //http://localhost:8080/students?id=1&firstname=Ryan&lastName=Thomas
     //User sends in ID, first name, and last name, then student object is returned
     @GetMapping("students/query")
-    public Student studentRequestMultipleVariable(
+    public ResponseEntity<Student> studentRequestMultipleVariable(
             @RequestParam int id,
             @RequestParam String firstName,
             @RequestParam String lastname){
-        return new Student(id, firstName, lastname);
+        Student student = new Student(id, firstName, lastname);
+        return ResponseEntity.ok(student);
     }
 
     //PathVariable versus RequestParam
@@ -71,28 +80,28 @@ public class StudentController {
     //Spring boot REST API that handles HTTP POST Request
     //Need @PostMapping and @RequestBody
     @PostMapping("students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student){
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);  //not using 'ok' because 'ok' returns 200, we need 201 here
     }
 
     //Put request to updating existing data
     @PutMapping("students/{id}/update")
-    public Student updateStudent(
+    public ResponseEntity<Student> updateStudent(
             @RequestBody Student student,  //remember that this grabs the JSON body and converts to Java object
             @PathVariable("id") int studentId){
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return ResponseEntity.ok(student);
     }
 
     //Delete data
     @DeleteMapping("students/{id}/delete")
-    public String deleteStudent(@PathVariable("id") int studentId){
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId){
         System.out.println(studentId);
-        return "Student deleted successfully!";
+        return ResponseEntity.ok("Student deleted successfully!");
     }
 }
