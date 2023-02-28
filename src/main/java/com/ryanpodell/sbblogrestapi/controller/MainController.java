b5c750d8fe5f5f300f50ef6ada1a3f1b0af7009d
rodell <ryanodell2668@gmail.com>
2023-02-28 16:05:55 -0500

    Package blueprint setup complete

--- src/main/java/com/ryanpodell/sbblogrestapi/controller/MainController.java
+++ src/main/java/com/ryanpodell/sbblogrestapi/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController // contains two annotations @controller and @restcontoller (@restcontroller -> object returned is automatically JSON and passed into HttpResponse object)
 public class MainController {
+    //All spring mvc classes will remain inside this package
 
     //HTTP GET Request
     //http://localhost:8080/hello-world
